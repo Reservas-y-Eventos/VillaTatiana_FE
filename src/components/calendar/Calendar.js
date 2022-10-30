bc45d8c9e567612455655ca159f80f5ae84338e2
Tania Pedraza <taniapedraza97@gmail.com>
2022-10-29 19:54:14 -0500

    New create reservation in calendar

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import FullCalendar from "@fullcalendar/react"
 import dayGridPlugin from "@fullcalendar/daygrid"
 import timeGridPlugin from "@fullcalendar/timegrid"
 import interactionPlugin from "@fullcalendar/interaction"
 import { Grid, GridCell } from "@rmwc/grid";
+import CalendarContext from "../../contexts/calendar-context";
 import "./calendar.css"
+import AddEvent from "./AddEvent";
 
 const Calendar = () => {
+    const { atrr, meth } = useContext(CalendarContext)
+    const { setOpen, setDateSelected } = meth
+
+    const handleDateSelect = (data) => {
+        console.log(data);
+        setDateSelected(data.startStr);
+        setOpen(true)
+    };
+
+    const handleEventClick = (clickInfo) => {
+    };
+
+    const handleEvents = (events) => {
+    };
     return (
         <Grid>
             <GridCell desktop={1} tablet={12} phone={12} />
@@ -24,10 +40,14 @@ const Calendar = () => {
                         selectable={true}
                         selectMirror={true}
                         dayMaxEvents={true}
+                        select={handleDateSelect}
+                        eventClick={handleEventClick}
+                        eventsSet={handleEvents}
                     />
                 </div>
             </GridCell>
             <GridCell desktop={1} tablet={12} phone={12} />
+            <AddEvent />
         </Grid>
     )
 }
