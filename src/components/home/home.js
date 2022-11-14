181e14a5b7581c6e011b4d427d5d7432e56dc721
Tania Pedraza <taniapedraza97@gmail.com>
2022-11-13 23:17:32 -0500

    Change hero description and component show

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -22,11 +22,11 @@ const Home = () => {
                         </Typography>
                         <br />
                         <Typography
-                            use="body1"
+                            use="subtitle1"
                             tag="div"
                             theme="textSecondaryOnBackground"
                         >
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            {t("homeDescription")}
                         </Typography>
                     </div>
                 </CardPrimaryAction>
